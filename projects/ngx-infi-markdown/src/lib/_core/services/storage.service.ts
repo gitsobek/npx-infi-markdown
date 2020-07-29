7de4e9737c15cf1917ca616bd7490b1a352c4aba
KacperG <brhelu@wp.pl>
2020-07-29 22:52:16 +0200

    refactor: create universal storage class exposed to inheritance

--- projects/ngx-infi-markdown/src/lib/_core/services/storage.service.ts
+++ projects/ngx-infi-markdown/src/lib/_core/services/storage.service.ts
@@ -1,44 +1,39 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class SessionStorageService implements StorageService {
+class StorageService {
+  private storage: Storage;
+
+  constructor(storage: Storage) {
+    this.storage = storage;
+  }
+
   getItem<T>(key: string): T {
-    return JSON.parse(sessionStorage.getItem(key));
+    return JSON.parse(this.storage.getItem(key));
   }
 
   setItem<T>(key: string, value: any): void {
-    return sessionStorage.setItem(key, JSON.stringify(value));
+    return this.storage.setItem(key, JSON.stringify(value));
   }
 
   removeItem(key: string): void {
-    return sessionStorage.removeItem(key);
+    return this.storage.removeItem(key);
   }
 }
 
 @Injectable({
   providedIn: 'root',
 })
-export class LocalStorageService implements StorageService {
-  getItem<T>(key: string): T {
-    return JSON.parse(localStorage.getItem(key));
-  }
-
-  setItem<T>(key: string, value: any): void {
-    return localStorage.setItem(key, JSON.stringify(value));
-  }
-
-  removeItem(key: string): void {
-    return localStorage.removeItem(key);
+export class SessionStorageService extends StorageService {
+  constructor() {
+    super(sessionStorage);
   }
 }
 
 @Injectable({
   providedIn: 'root',
 })
-export abstract class StorageService {
-  abstract getItem<T>(key: string): T;
-  abstract setItem<T>(key: string, value: any): void;
-  abstract removeItem(key: string): void;
+export class LocalStorageService extends StorageService {
+  constructor() {
+    super(localStorage);
+  }
 }
